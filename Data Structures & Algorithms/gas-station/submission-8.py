9a3b9286f87c8d2ba9740bdf8bf9e8b0fcb33df6
Nithin Puthalath Manoj <nithinp.manoj@gmail.com>
2026-09-15 11:09:23 +0530

    Add: gas-station - submission-8

---
+++ Data Structures & Algorithms/gas-station/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_gas = 0
+        total_cost = 0
+        total_stations = len(gas)
+
+        curr_station = 0
+        start_station = 0
+        gas_tank = 0
+
+        while curr_station < total_stations:
+            total_gas += gas[curr_station]
+            total_cost += cost[curr_station]
+
+            gas_tank += (gas[curr_station] - cost[curr_station])
+
+            if gas_tank < 0:
+                start_station = curr_station + 1
+                gas_tank = 0
+            
+            curr_station += 1
+
+        return -1 if total_cost > total_gas else start_station
